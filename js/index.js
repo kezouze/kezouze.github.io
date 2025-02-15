58b1a92fa8665541594c19d73d87a46188f3215b
kezouze <vincent-c51@hotmail.fr>
2025-02-15 11:44:09 +0100

    Simplified index.js, prev end next btns while fullscreen also on index.hmtl

--- js/index.js
+++ js/index.js
@@ -1,26 +1,6 @@
+import { handleImageLoading, setupModal } from "./utils.js";
+handleImageLoading();
+
 const galleryImages = document.querySelectorAll('#gallery .grid-item img');
-const fullscreenImage = document.getElementById('fullscreenImage');
-const imageModal = new bootstrap.Modal(document.getElementById('imageModal'));
+setupModal(galleryImages);
 
-galleryImages.forEach(image => {
-    image.addEventListener('click', function () {
-        fullscreenImage.src = this.src;
-        imageModal.show();
-    });
-});
-
-const blurredImageDivs = document.querySelectorAll(".blurred-img");
-
-blurredImageDivs.forEach((blurredImageDiv) => {
-    const img = blurredImageDiv.querySelector("img")
-
-    function loaded() {
-        blurredImageDiv.classList.add("loaded")
-    }
-
-    if (img.complete) {
-        loaded()
-    } else {
-        img.addEventListener("load", loaded)
-    }
-});
